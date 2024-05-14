f31e8a1e5a2733f75534c4e7c925044165c4fac7
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-05-14 14:47:03 -0500

    Update Experiments

--- backend_structures/experiments/avlExperiment.cpp
+++ backend_structures/experiments/avlExperiment.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <chrono>
 #include <string>
-#include <vector>
 #include <cstring>
 #include "avlfile.cpp"
 
@@ -10,7 +9,7 @@ using namespace std::chrono;
 
 #define ll long long
 
-void test(AVLFile<int> &file, ll dataSize) {
+void fulfill(AVLFile<int> &file, ll dataSize) {
 	file.clear();
 
 	for (int i = 0; i < dataSize; i++) {
@@ -26,37 +25,72 @@ void test(AVLFile<int> &file, ll dataSize) {
 	}
 }
 
+void testSingleSearch(AVLFile<int> &avlfile, int key) {
+	cout << "Busqueda de un registro" << endl;
+
+	auto start = high_resolution_clock::now();
+	auto records = avlfile.search(key);
+	auto end = high_resolution_clock::now();
+	auto time_span = duration_cast<microseconds>(end - start);
+
+	for (auto r : records)
+		cout << r.cod << " " << r.nombre << " " << endl;
+
+	cout << "Tiempo de busqueda: " << time_span.count() << " ms" << endl;
+	cout << "################################" << endl << endl;
+}
+
+void testRangeSearch(AVLFile<int> &avlfile, int beginKey, int endKey) {
+	cout << "Busqueda de un rango de registros" << endl;
+
+	auto start = high_resolution_clock::now();
+	auto records = avlfile.rangeSearch(beginKey, endKey);
+	auto end = high_resolution_clock::now();
+	auto time_span = duration_cast<microseconds>(end - start);
+
+	for (auto r : records)
+		cout << r.cod << " " << r.nombre << " " << endl;
+
+	cout << "Tiempo de busqueda por rango: " << time_span.count() << " ms" << endl;
+	cout << "################################" << endl << endl;
+}
+
+void testInsert(AVLFile<int> &avlfile, int key) {
+	cout << "Insercion de un registro" << endl;
+
+	Record<int> r = {key, "Name", 1};
+
+	auto start = high_resolution_clock::now();
+	avlfile.insert(r);
+	auto end = high_resolution_clock::now();
+	auto time_span = duration_cast<microseconds>(end - start);
+
+	cout << "Tiempo de insercion: " << time_span.count() << " ms" << endl;
+	cout << "################################" << endl << endl;
+}
+
+void testDelete(AVLFile<int> &avlfile, int key) {
+	cout << "Eliminacion de un registro" << endl;
+
+	auto start = high_resolution_clock::now();
+	cout << avlfile.remove(key);
+	auto end = high_resolution_clock::now();
+	auto time_span = duration_cast<microseconds>(end - start);
+
+	cout << "Tiempo de eliminacion: " << time_span.count() << " ms" << endl;
+	cout << "################################" << endl << endl;
+}
 
 int main(){
 	AVLFile<int> avlfile("data.dat");
-	ll dataSize = 1000;
+	ll dataSize = 10000;
 
-	test(avlfile, dataSize);
+//	fulfill(avlfile, dataSize);
 
-	time_point<high_resolution_clock> start, end;
-
-	// Fix this
-//	duration<double> time_span;
-
-	cout << "Busqueda de un registro" << endl;
-
-	start = high_resolution_clock::now();
-	auto records = avlfile.search(63);
-	end = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(end - start);
-
-	cout << "Tiempo de busqueda " << duration.count() << " ms" << endl;
-
-	// #############################################################3
-
-//	cout << "Búsqueda de un rango de registros" << endl;
-
-//	start = high_resolution_clock::now();
-//	auto records2 = avlfile.rangeSearch(63, 70);
-//	end = high_resolution_clock::now();
-
-
-
+//	testSingleSearch(avlfile, 1);
+//	testRangeSearch(avlfile, 5555, 5558);
+//	testInsert(avlfile, 10001);
+//	testDelete(avlfile, 3333);
 
 	return 0;
 }
