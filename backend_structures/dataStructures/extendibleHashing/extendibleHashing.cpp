73586969e34cc593ed9166215ea0afdf628fc2c5
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-05-16 01:00:11 -0500

    ☠️💀

--- backend_structures/dataStructures/extendibleHashing/extendibleHashing.cpp
+++ backend_structures/dataStructures/extendibleHashing/extendibleHashing.cpp
@@ -5,20 +5,23 @@
 #include <iomanip>
 #include <cstring>
 
-const int globalD = 3;
-const int maxSize = 3;
+const int globalDepth = 3;
+const int blockFactor = 3;
 
 using namespace std;
+
+
+void SAFE_FILE_OPEN(){
+}
 
 struct Record {
 	int key;
 	char nombre[12];
-	int ciclo;
 };
 
 struct Bucket {
 	int bucketId{};
-	Record records[maxSize]{};
+	Record records[blockFactor]{};
 	int locald;
 	int size;
 	int nextBucket;
@@ -28,84 +31,120 @@ struct Bucket {
 		size = 0;
 		nextBucket = -1;
 	}
+
+	bool isFull(){
+		return (size == locald);
+	}
+
+	void add(Record new_record){
+		if(!isFull()){
+			records[size++] = new_record;
+		}
+	}
+
+	void clear(){
+		for (int i = 0; i < size; ++i) {
+			records[i] = Record();
+		}
+		size = 0;
+	}
+
+	bool isInBucket(int key){
+		for (int i = 0; i < size; i++) {
+			if (this->records[i].key == key){
+				return (true);
+			};
+		}
+		return false;
+	}
+
+	Record search(int key){
+		if is
+	}
 };
 
+
+
+
 struct addressRecord {
-	char hashIndex[globalD]{};
+	char binaryIndex[globalDepth]{};
 	int bucketIndex;
 	int locald = 1;
 
-//	addressRecord(string hashIndex, int bucketIndex){
-//		this->locald = 1;
-//
-//		strncpy(this->hashIndex, hashIndex.c_str(), 3);
-//
-//		this->bucketIndex = bucketIndex;
-//	}
-//
-//	addressRecord(){
-//		this->locald = 1;
-//		this->bucketIndex = -1;
-//	}
+
 };
 
-class ExtendibleHashing {
+class ExtendibleHash {
 private:
+	
 	string hashFile;
 	string addressTable;
 	int nBuckets;
 public:
-	ExtendibleHashing(string hashFile, string addressTable) {
+	ExtendibleHash(string addressTable, string hashFile){
 		this->hashFile = hashFile;
 		this->addressTable = addressTable;
 
-		fstream addresstable(addressTable, ios::binary | ios::in | ios::app);
 
-		if (!addresstable.is_open())
-			cout << "Error opening address table" << endl;
-
-		if (addresstable.peek() != ifstream::traits_type::eof()){
-			addresstable.seekg(0, ios::beg);
-			addresstable.read((char*) &nBuckets, sizeof(int));
-		} else {
-			cout << "Creating address table" << endl;
-			addressRecord addressRecord0;
-			addressRecord0.hashIndex[0] = '0';
-			addressRecord0.bucketIndex = 0;
-			addressRecord addressRecord1;
-			addressRecord1.hashIndex[0] = '1';
-			addressRecord1.bucketIndex = 1;
-
-			this->nBuckets = 2;
-			addresstable.seekg(0, ios::beg);
-			addresstable.write((char*) &nBuckets, sizeof(int));
-			addresstable.write((char*) &addressRecord0, sizeof(addressRecord));
-			addresstable.write((char*) &addressRecord1, sizeof(addressRecord));
-		}
-		addresstable.close();
 	}
 
-	void printAll() {
-		ifstream addresstable(addressTable, ios::in | ios::binary);
 
-		cout << "Address Table" << endl;
-		addresstable.seekg(0, ios::beg);
-		addresstable.read((char*) &nBuckets, sizeof(int));
-		cout << "Number of buckets: " << nBuckets << endl;
 
-		addressRecord addressR;
-		while(addresstable.read((char*) &addressR, sizeof(addressRecord))) {
-			cout << addressR.hashIndex << " " << addressR.bucketIndex << endl;
-		}
 
-		addresstable.close();
-	}
+
+
+
+
+//	ExtendibleHashing(string hashFile, string addressTable) {
+//		this->hashFile = hashFile;
+//		this->addressTable = addressTable;
+//
+//		fstream addresstable(addressTable, ios::binary | ios::in | ios::app);
+//
+//		if (!addresstable.is_open())
+//			cout << "Error opening address table" << endl;
+//
+//		if (addresstable.peek() != ifstream::traits_type::eof()){
+//			addresstable.seekg(0, ios::beg);
+//			addresstable.read((char*) &nBuckets, sizeof(int));
+//		} else {
+//			cout << "Creating address table" << endl;
+//			addressRecord addressRecord0;
+//			addressRecord0.hashIndex[0] = '0';
+//			addressRecord0.bucketIndex = 0;
+//			addressRecord addressRecord1;
+//			addressRecord1.hashIndex[0] = '1';
+//			addressRecord1.bucketIndex = 1;
+//
+//			this->nBuckets = 2;
+//			addresstable.seekg(0, ios::beg);
+//			addresstable.write((char*) &nBuckets, sizeof(int));
+//			addresstable.write((char*) &addressRecord0, sizeof(addressRecord));
+//			addresstable.write((char*) &addressRecord1, sizeof(addressRecord));
+//		}
+//		addresstable.close();
+//	}
+//
+//	void printAll() {
+//		ifstream addresstable(addressTable, ios::in | ios::binary);
+//
+//		cout << "Address Table" << endl;
+//		addresstable.seekg(0, ios::beg);
+//		addresstable.read((char*) &nBuckets, sizeof(int));
+//		cout << "Number of buckets: " << nBuckets << endl;
+//
+//		addressRecord addressR;
+//		while(addresstable.read((char*) &addressR, sizeof(addressRecord))) {
+//			cout << addressR.hashIndex << " " << addressR.bucketIndex << endl;
+//		}
+//
+//		addresstable.close();
+//	}
 };
 
 int main(){
-	ExtendibleHashing hash("addressTable.dat", "addressTable.dat");
-
-	hash.printAll();
+//	ExtendibleHashing hash("addressTable.dat", "addressTable.dat");
+//	hash.printAll();
 
 	return 0;
 }
