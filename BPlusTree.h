b34832c4a0eeb0f12c037c21e4240545ca993d0e
mukanjy0 <mukanjyo29@protonmail.com>
2024-05-18 20:11:59 -0500

    added removal

--- BPlusTree.h
+++ BPlusTree.h
@@ -136,7 +136,7 @@ class BPlusTree {
         return node;
     }
 
-    Node _splitChild(Node x, int pos, int i) {
+    Node _splitChild(Node& x, int pos, int i) {
         Node y = getNode(x.c[i]);
         Node z {};
         // move y's last t-1 keys to z
@@ -171,7 +171,7 @@ class BPlusTree {
         return x;
     }
 
-    Node _splitLeafChild(Node x, int pos, int i) {
+    Node _splitLeafChild(Node& x, int pos, int i) {
         Leaf y = getLeaf(x.c[i]);
         Leaf z {};
         int zPos = allocatePos();
@@ -216,7 +216,7 @@ class BPlusTree {
         _splitChild(s, root, 0);
     }
 
-    bool _insertNonFullLeaf(Leaf x, int pos, Key key, Record& record) {
+    bool _insertNonFullLeaf(Leaf& x, int pos, Key key, Record& record) {
         int match {};
         for (int k = x.n - 1; k > 0; k >>= 1)
             while (match + k < x.n && getKey(x.records[match + k]) <= key) match += k;
@@ -231,7 +231,7 @@ class BPlusTree {
         return true;
     }
 
-    bool _insertNonFull(Node x, int pos, Key key, Record& record) {
+    bool _insertNonFull(Node& x, int pos, Key key, Record& record) {
         // find child to insert into
         int i {};
         for (; i < x.n; ++i)
@@ -255,6 +255,222 @@ class BPlusTree {
             }
             return _insertNonFull(child, x.c[i], key, record);
         }
+    }
+
+    pair<Key,bool> _removeFromLeaf(Leaf& x, int pos, Key key) { // returns {predecessor/successor, wasRemoved}
+        int match {};
+        for (int k = x.n - 1; k > 0; k >>= 1)
+            while (match + k < x.n && getKey(x.records[match + k]) <= key) match += k;
+        if (getKey(x.records[match]) != key) return {Key{},false};
+
+        Key replacement {};
+        if (match == 0) replacement = getKey(x.records[1]);
+        else replacement = getKey(x.records[0]);
+
+        for (int i = match + 1; i < x.n; ++i)
+            x.records[i - 1] = x.records[i];
+        x.n -= 1;
+
+        setLeaf(x, pos);
+
+        return {replacement, true};
+    }
+
+    void getKeyFromLeft(Node& x, int pos, int i) {
+        if (x.leaf) {
+            Leaf child = getLeaf(x.c[i]);
+            Leaf leftChild = getLeaf(x.c[i - 1]);
+            for (int j = child.n; j > 0; --j)
+                child.records[j] = child.records[j - 1];
+
+            child.records[0] = leftChild.records[leftChild.n - 1];
+            child.n += 1;
+            leftChild.n -= 1;
+
+            x.k[i - 1] = getKey(child.records[0]);
+
+            setNode(x, pos);
+            setLeaf(leftChild, x.c[i - 1]);
+            setLeaf(child, x.c[i]);
+        }
+        else {
+            Node child = getNode(x.c[i]);
+            Node leftChild = getNode(x.c[i - 1]);
+
+            for (int j = child.n; j > 0; --j)
+                child.k[j] = child.k[j - 1];
+            for (int j = child.n + 1; j > 0; --j)
+                child.c[j] = child.c[j - 1];
+
+            child.k[0] = x.k[i];
+            child.c[0] = leftChild.c[leftChild.n];
+            x.k[i - 1] = leftChild.k[leftChild.n - 1];
+
+            child.n += 1;
+            leftChild.n -= 1;
+
+            setNode(x, pos);
+            setNode(child, x.c[i]);
+            setNode(leftChild, x.c[i - 1]);
+        }
+    }
+
+    void getKeyFromRight(Node& x, int pos, int i) {
+        if (x.leaf) {
+            Leaf child = getLeaf(x.c[i]);
+            Leaf rightChild = getLeaf(x.c[i + 1]);
+            child.records[child.n] = rightChild.records[0];
+            for (int j = 1; j < rightChild.n; ++j)
+                rightChild.records[j - 1] = rightChild.records[j];
+
+            child.n += 1;
+            rightChild.n -= 1;
+
+            x.k[i] = rightChild.records[0];
+
+            setNode(x, pos);
+            setLeaf(rightChild, x.c[i + 1]);
+            setLeaf(child, x.c[i]);
+        }
+        else {
+            Node child = getNode(x.c[i]);
+            Node rightChild = getNode(x.c[i + 1]);
+
+            child.k[child.n] = x.k[i];
+            x.k[i] = rightChild.k[0];
+            for (int j = 1; j < rightChild.n; ++j)
+                rightChild.k[j - 1] = rightChild.k[j];
+
+            child.c[child.n + 1] = rightChild.c[0];
+            for (int j = 1; j <= rightChild.n; ++j)
+                rightChild.c[j - 1] = rightChild.c[j];
+
+            child.n += 1;
+            rightChild.n -= 1;
+
+            setNode(x, pos);
+            setNode(child, x.c[i]);
+            setNode(rightChild, x.c[i + 1]);
+        }
+    }
+
+    void mergeWithLeft(Node& x, int pos, int i) {
+        if (x.leaf) {
+           Leaf child = getLeaf(x.c[i]);
+           Leaf leftChild = getLeaf(x.c[i - 1]);
+
+           for (int j = 0; j < child.n; ++j)
+               leftChild.records[j + leftChild.n] = child.records[j];
+
+            Leaf del {};
+            del.nextDel = lastDel;
+            lastDel = x.c[i];
+
+           for (int j = i; j < x.n; ++j)
+               x.k[i - 1] = x.k[i];
+
+           for (int j = i + 1; j <= x.n; ++j)
+               x.c[j - 1] = x.c[j];
+
+           x.n -= 1;
+           leftChild.n += child.n;
+
+            setNode(x, pos);
+            setLeaf(leftChild, x.c[i - 1]);
+            setLeaf(del, lastDel);
+            setHeader();
+        }
+        else {
+            Node child = getNode(x.c[i]);
+            Node leftChild = getNode(x.c[i - 1]);
+
+            leftChild.k[leftChild.n] = x.k[i];
+            for (int j = 1; j <= child.n; ++j)
+                leftChild.k[j + leftChild.n] = child[j - 1];
+
+            for (int j = 1; j <= child.n + 1; ++j)
+                leftChild.c[j + leftChild.n] = child.c[j - 1];
+
+            Leaf del {};
+            del.nextDel = lastDel;
+            lastDel = x.c[i];
+
+            for (int j = i; j < x.n; ++j)
+                x.k[i - 1] = x.k[i];
+
+            for (int j = i + 1; j <= x.n; ++j)
+                x.c[j - 1] = x.c[j];
+
+            x.n -= 1;
+            leftChild.n += child.n;
+
+            setNode(x, pos);
+            setNode(leftChild, x.c[i - 1]);
+            setLeaf(del, lastDel);
+            setHeader();
+        }
+    }
+
+    pair<Key,bool> _remove(Node& x, int pos, Key key) {
+        // find child to recurse into
+        int i {}, m {};
+        for (; i < x.n; ++i)
+            if (x.k[i] > key) break;
+
+        Key replacement {};
+        bool wasRemoved {};
+        if (x.leaf) {
+            m = (blockingFactor>>1);
+            Leaf child = getLeaf(x.c[i]);
+            if (child.n == m) {
+                Leaf leftChild {};
+                Leaf rightChild {};
+                if (i > 0) leftChild = getLeaf(x.c[i - 1]);
+                if (i < x.n) rightChild = getLeaf(x.c[i + 1]);
+
+                if (leftChild.n > m) getKeyFromLeft(x, pos, i);
+                else if (rightChild.n > m) getKeyFromRight(x, pos, i);
+                else if (i > 0) {
+                    mergeWithLeft(x, pos, i);
+                    i -= 1;
+                }
+                else mergeWithLeft(x, pos, i + 1);
+            }
+            child = getLeaf(x.c[i]);
+
+            auto p = _removeFromLeaf(child, x.c[i], key);
+            replacement = p.fi;
+            wasRemoved = p.se;
+        }
+        else {
+            m = t - 1;
+            Node child = getNode(x.c[i]);
+            if (child.n == m) {
+                Node leftChild {};
+                Node rightChild {};
+                if (i > 0) leftChild = getNode(x.c[i - 1]);
+                if (i < x.n) rightChild = getNode(x.c[i + 1]);
+
+                if (leftChild.n > m) getKeyFromLeft(x, pos, i);
+                else if (rightChild.n > m) getKeyFromRight(x, pos, i);
+                else if (i > 0) {
+                    mergeWithLeft(x, pos, i);
+                    i -= 1;
+                }
+                else mergeWithLeft(x, pos, i + 1);
+            }
+            child = getNode(x.c[i]);
+
+            auto p = _remove(child, x.c[i], key);
+            replacement = p.fi;
+            wasRemoved = p.se;
+        }
+
+        if (wasRemoved && i > 0 && x.k[i - 1] == key) {
+            x.k[i - 1] = replacement;
+            setNode(x, pos);
+        }
+        return {replacement, wasRemoved};
     }
 
     pair<Record,bool> _search(Leaf x, Key key) {
@@ -312,7 +528,7 @@ public:
 
         // if root has no keys, create key and leaf + add record
         Node r = getNode(root);
-        if (r.n == 0) {
+        if (r.c[0] == 0) {
             int lPos = allocatePos();
             int rPos = allocatePos();
             if (rPos == lPos) ++rPos;
@@ -343,7 +559,9 @@ public:
     }
 
     bool remove(Key key) {
-
+        Node r = getNode(root);
+        auto [replacement, wasRemoved] = _remove(r, root, key);
+        return wasRemoved;
     }
 
     vector<Record> range(Key left, Key right) {
